33b225d719e04077665ac4b93c8bc13102eb9804
notjesseolsson <notjesseolsson@gmail.com>
2014-12-02 18:54:16 -0500

    bump textures and broken level 1

--- _site/outstandingtux/index.js
+++ _site/outstandingtux/index.js
@@ -1,4 +1,27 @@
 (function() {
-
+  document.addEventListener('DOMContentLoaded', function() {
+    var canvas, engine;
+    canvas = document.querySelector('canvas');
+    engine = new BABYLON.Engine(canvas, true);
+    return BABYLON.SceneLoader.Load('', 'levels.babylon', engine, function(scene) {
+      return scene.executeWhenReady(function() {
+        var level1, ocean;
+        scene.activeCamera.attachControl(canvas);
+        ocean = scene.getMeshByName('ocean');
+        level1 = scene.getMeshByName('level.1');
+        level1.material.emissiveTexture = new BABYLON.Texture('level.png', scene);
+        level1.material.specularPower = 3;
+        level1.material.bumpTexture = new BABYLON.Texture('level_NRM.png', scene);
+        level1.material.emissiveColor = new BABYLON.Color3(1, 1, 1);
+        return engine.runRenderLoop(function() {
+          scene.render();
+          ocean.material.diffuseTexture.uOffset += .001;
+          ocean.material.diffuseTexture.vOffset += .0008;
+          ocean.material.bumpTexture.uOffset += .001;
+          return ocean.material.bumpTexture.vOffset += .0008;
+        });
+      });
+    });
+  });
 
 }).call(this);
--- outstandingtux/index.coffee
+++ outstandingtux/index.coffee
@@ -1,2 +1,25 @@
 ---
 ---
+
+document.addEventListener('DOMContentLoaded', ->
+  canvas = document.querySelector('canvas')
+  engine = new BABYLON.Engine(canvas, true)
+  BABYLON.SceneLoader.Load('', 'levels.babylon', engine, (scene) ->
+    scene.executeWhenReady(->
+      scene.activeCamera.attachControl(canvas)
+      ocean = scene.getMeshByName('ocean')
+      level1 = scene.getMeshByName('level.1')
+      level1.material.emissiveTexture = new BABYLON.Texture('level.png', scene)
+      level1.material.specularPower = 3
+      level1.material.bumpTexture = new BABYLON.Texture('level_NRM.png', scene)
+      level1.material.emissiveColor = new BABYLON.Color3(1, 1, 1)
+      engine.runRenderLoop(->
+        scene.render()
+        ocean.material.diffuseTexture.uOffset += .001
+        ocean.material.diffuseTexture.vOffset += .0008
+        ocean.material.bumpTexture.uOffset += .001
+        ocean.material.bumpTexture.vOffset += .0008
+      )
+    )
+  )
+)
